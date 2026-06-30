55667e7e2ddbb1347c2a722b2faeec1131b8f05b
SyFyz <embrizo@hotmail.com>
2026-07-01 00:12:31 +0700

    atm fix

--- python-learning/atm.py
+++ python-learning/atm.py
@@ -5,22 +5,45 @@
 #2. Deposit
 #3. Withdraw
 #4. Exit
-
-user = input("Enter your username: ")
-password = input("Enter your password: ")
-
-authen = False
-
-if user == "admin" and password == "1234":
-    print("Login success")
-    authen = True
-else:
-    print("Login failed")
-    authen = False
 balance = 10000
 history = []
 
-if authen == True:
+def check_balance():
+    print(f"Balance: {balance:.2f}")
+
+def login():
+    user = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if user == "admin" and password == "1234":
+        print("Login success")
+        return True
+    else:
+        print("Login failed")
+        return False
+
+def deposit():
+    global balance
+    deposit_amount = float(input("Enter amount to deposit: "))
+    balance += deposit_amount
+    history.append(f"Deposit: {deposit_amount}")
+    print(f"Balance: {balance:.2f}")
+
+def withdraw():
+    global balance
+    withdraw_amount = float(input("Enter amount to withdraw: "))
+    if withdraw_amount > balance:
+        print("Insufficient balance")
+    else:
+        balance -= withdraw_amount
+        history.append(f"Withdraw: {withdraw_amount}")
+        print(f"Balance: {balance:.2f}")
+
+def show_history():
+    for item in history:
+        print(item)
+
+
+if login():
     print("Welcome to the ATM system")
     while True:
         print("1. Check Balance")
@@ -30,22 +53,13 @@ if authen == True:
         print("5. Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
-            print(f"Balance: {balance}")
+            check_balance()
         elif choice == "2":
-            depo = float(input("Enter amount to deposit: "))
-            balance += depo
-            history.append(f"Deposit: {depo}")
-            print(f"Balance: {balance}")
+            deposit()
         elif choice == "3":
-            withdr = float(input("Enter amount to withdraw: "))
-            if withdr > balance:
-                print("Insufficient balance")
-            else:
-                balance -= withdr
-                history.append(f"Withdraw: {withdr}")
-                print(f"Balance: {balance}")
+            withdraw()
         elif choice == "4":
-            print(f"History: {history}")
+            show_history()
         elif choice == "5":
             print("Exit Goodbye")
             break
